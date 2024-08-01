b876356df3e9eb4b99b9ab082169721d8731f7cf
Aditya Sarkar <adityasarkar2706@gmail.com>
2024-08-01 19:57:38 +0530

    hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+import pickle
 
 #sys.path.insert(0, '/home/brucewayne/Documents/Work_1/src')
 from src.logger import logging
@@ -21,15 +23,23 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(X_train,y_train,X_test,y_test,model):
+def evaluate_models(X_train,y_train,X_test,y_test,model,param):
     #models = list(model)
     report = {}
     try:
         for i in range(len(list(model))):
             c_model = list(model.values())[i]
+            para = param[list(model.keys())[i]]
+            
+            gs = GridSearchCV(c_model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            c_model.set_params(**gs.best_params_)
             c_model.fit(X_train,y_train)
+
             y_train_pred= c_model.predict(X_train)
             y_test_pred = c_model.predict(X_test)
+            
             train_model_score = r2_score(y_train,y_train_pred)
             test_model_score = r2_score(y_test,y_test_pred)
             report[(list(model.keys()))[i]] = test_model_score
@@ -37,6 +47,14 @@ def evaluate_models(X_train,y_train,X_test,y_test,model):
         return report
     except Exception as e:
         raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
 
         
 
